8b011fc8795fe72d9bf9c17e91ee289deb162cb8
Simon Klimov <simon-klimov@yandex.ru>
2019-03-24 22:08:17 +0300

    Added OddOccurrencesInArray task

--- Codility/Program.cs
+++ Codility/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using static BinaryGap.Program;
 using static CyclicRotation.Program;
+using static OddOccurrencesInArray.Program;
 
 
 namespace Codility
@@ -29,6 +30,14 @@ namespace Codility
 			Console.WriteLine(ArrayToString(CyclicRotate(new int[] { -100, -1, 0, 1, 6 }, 6)));
 			Console.WriteLine(ArrayToString(CyclicRotate(new int[] { -100, -1, 0, 1, 6 }, 10)));
 			Console.WriteLine(ArrayToString(CyclicRotate(new int[] { }, 10)));
+
+			// OddOccurrencesInArray
+			Console.WriteLine(FindOddOccurrenceInArray(new int[] { 9, 3, 9, 3, 9, 7, 9 }));
+			Console.WriteLine(FindOddOccurrenceInArray(new int[] { 4, 7, 6, 9, 5, 7, 4, 9, 6 }));
+			Console.WriteLine(FindOddOccurrenceInArray(new int[] { 0 }));
+			Console.WriteLine(FindOddOccurrenceInArray(new int[] { 1 }));
+			Console.WriteLine(FindOddOccurrenceInArray(new int[] { 0, 0, 1 }));
+			Console.WriteLine(FindOddOccurrenceInArray(new int[] { 0, 1, 2, 3, 4, 5, 4, 3, 2, 1, 0 }));
 
 		}
 	}
---
+++ OddOccurrencesInArray/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace OddOccurrencesInArray
+{
+	public class Program
+	{
+		public static int FindOddOccurrenceInArray(int[] A)
+		{
+			foreach (int i in A.Distinct().ToList())
+				if (Array.FindAll(A, n => n == i).Length % 2 == 1)
+					return i;
+			return 0;
+		}
+
+		static void Main(string[] args)
+		{
+			Console.WriteLine(FindOddOccurrenceInArray(new int[] { 9, 3, 9, 3, 9, 7, 9 }));
+			Console.WriteLine(FindOddOccurrenceInArray(new int[] { 4, 7, 6, 9, 5, 7, 4, 9, 6 }));
+			Console.WriteLine(FindOddOccurrenceInArray(new int[] { 0 }));
+			Console.WriteLine(FindOddOccurrenceInArray(new int[] { 1 }));
+			Console.WriteLine(FindOddOccurrenceInArray(new int[] { 0,0,1 }));
+			Console.WriteLine(FindOddOccurrenceInArray(new int[] { 0, 1, 2, 3, 4, 5, 4, 3, 2, 1, 0 }));
+
+		}
+	}
+}
