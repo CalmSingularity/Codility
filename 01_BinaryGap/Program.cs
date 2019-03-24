51e1a82d2624c5e2cd85342e2c48d315a0ec5b2a
Simon Klimov <simon-klimov@yandex.ru>
2019-03-24 18:03:06 +0300

    Add project files.

---
+++ 01_BinaryGap/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace BinaryGap
+{
+	public class Program
+	{
+		public static int MaxBinaryGap(int N)
+		{
+			int maxGap = 0;
+			int currentGap = -1; // no gap found yet
+
+			while (N > 0)
+			{
+				if ((N & 1) == 1)  // rightmost bit == 1
+				{
+					if (currentGap == -1)
+					{
+						currentGap = 0;  // start counting the gap
+					}
+					else 
+					{
+						if (currentGap > maxGap)
+							maxGap = currentGap;
+						currentGap = 0;  // reset the current gap as we met 1
+					}
+				}
+				else if (currentGap >= 0)  // rightmost bit == 0 && current gap != -1 (we've met at least one 1)
+				{
+					++currentGap;
+				}
+				N = N >> 1;  // right bit shift to evaluate the next bit in the next while cycle
+			}
+			return maxGap;
+		}
+
+		static void Main(string[] args)
+		{
+			Console.WriteLine(MaxBinaryGap(529));
+			Console.WriteLine(MaxBinaryGap(579));
+			//...
+		}
+	}
+}
---
+++ Codility/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Codility
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			// Lesson 1. Iterations
+			Console.WriteLine(BinaryGap.Program.MaxBinaryGap(5));
+			Console.WriteLine(BinaryGap.Program.MaxBinaryGap(9));
+			Console.WriteLine(BinaryGap.Program.MaxBinaryGap(32));
+			Console.WriteLine(BinaryGap.Program.MaxBinaryGap(529));
+			Console.WriteLine(BinaryGap.Program.MaxBinaryGap(549));
+
+
+		}
+	}
+}
